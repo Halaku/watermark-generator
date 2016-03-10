9f3bf07a41fe512d28d7d0caff339c9548a16d36
Natasha Hivoyno <xixi.ru@mail.ru>
2016-03-10 10:43:04 +0300

    пересчет размеров изображений при загрузке

--- app/js/positioning.js
+++ app/js/positioning.js
@@ -6,47 +6,63 @@ var Positioning = (function(){
 			inputY = $('#coords-y'),
 			img = $('.processed_img'),
 			wm = $('.processed_wm'),
-			imgWidth = img.width(),
-			imgHeight = img.height(),
-			wmWidth = wm.width(),
-			wmHeight = wm.height();
-
-//Координаты для секторов
-	var displacement = [
-			[0, 0],
-			[0, (imgWidth/2) - (wmWidth/2)],
-			[0, imgWidth - wmWidth],
-			[(imgHeight/2) - (wmHeight/2), 0],
-			[(imgHeight/2) - (wmHeight/2), (imgWidth/2) - (wmWidth/2)],
-			[(imgHeight/2) - (wmHeight/2), imgWidth - wmWidth],
-			[imgHeight - wmHeight, 0],
-			[imgHeight - wmHeight, (imgWidth/2) - (wmWidth/2)],
-			[imgHeight - wmHeight, imgWidth - wmWidth]
-	];
+			imgWidth,
+			imgHeight,
+			wmWidth,
+			wmHeight,
+			displacement;
 
 	var init = function(){
+		_sizesUpdate();
 		_setUpListners();
-	};
-
-	var _setUpListners = function(){
-		mapList.on('click', _sector);
 		_coord();
 		_mouseMove();
 		_coordSpin();
 	};
 
+	var _setUpListners = function(){
+		mapList.on('click', _sector);
+	};
+
+	var _sizesUpdate = function(){
+		img.load(function() {
+  		imgWidth = img.width(),
+			imgHeight = img.height();
+		});
+
+		wm.load(function() {
+			wmWidth = wm.width(),
+			wmHeight = wm.height();
+		});
+
+		mapList.on('click', function(){
+			//Координаты для секторов
+			displacement = [
+					[0, 0],
+					[0, (imgWidth/2) - (wmWidth/2)],
+					[0, imgWidth - wmWidth],
+					[(imgHeight/2) - (wmHeight/2), 0],
+					[(imgHeight/2) - (wmHeight/2), (imgWidth/2) - (wmWidth/2)],
+					[(imgHeight/2) - (wmHeight/2), imgWidth - wmWidth],
+					[imgHeight - wmHeight, 0],
+					[imgHeight - wmHeight, (imgWidth/2) - (wmWidth/2)],
+					[imgHeight - wmHeight, imgWidth - wmWidth]
+			];
+		});
+	};
+
 //Перемещение вотермарка с помощью секторов
 	var _sector = function(e){
 		$(mapItem).removeClass('map__item_active');
-		for(var i=0; i<mapItem.length; i++){
-			if(e.target == mapItem[i]){
-				wm.css({'top': displacement[i][0],
-								'left':displacement[i][1]});
-				inputX.val(displacement[i][1]);
-				inputY.val(displacement[i][0]);
-				$(mapItem[i]).addClass('map__item_active');
+			for(var i=0; i<mapItem.length; i++){
+				if(e.target == mapItem[i]){
+					wm.css({'top': displacement[i][0],
+									'left':displacement[i][1]});
+					inputX.val(displacement[i][1]);
+					inputY.val(displacement[i][0]);
+					$(mapItem[i]).addClass('map__item_active');
+				}
 			}
-		}
 	};
 
 //Перемещение вотермарка с помощью инпутов
@@ -79,22 +95,26 @@ var Positioning = (function(){
 
 //Перемещение вотермарка спиннерами
 	var _coordSpin = function(){
-		var spinnerX = $( "#coords-x" ).spinner({
-			min: 0,
-			max: imgWidth-wmWidth
-		});
+		var maxWidth = imgWidth-wmWidth;
+		var spinnerX = inputX.spinner();
 		spinnerX.on('spin', function(e, ui){
+			spinnerX = inputX.spinner({
+				min: 0,
+				max: imgWidth-wmWidth
+			});
+			console.log(imgHeight-wmHeight);
 			var currentVal = ui.value;
 			wm.css({
 				left: currentVal + 'px'
 			});
 		});
 
-		var spinnerY =$( "#coords-y" ).spinner({
-			min: 0,
-			max: imgHeight-wmHeight
-		});
+		var spinnerY = inputY.spinner();
 		spinnerY.on('spin', function(e, ui){
+			spinnerY = inputY.spinner({
+				min: 0,
+				max: imgHeight-wmHeight
+			});
 			var currentVal = ui.value;
 			wm.css({
 				top: currentVal + 'px'
